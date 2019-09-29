150e4716a2d1a6fc8198553aedb5fdd756a703f4
Marc Daniel Morales Barreda <mmoralesbarreda@gmail.com>
2019-09-29 11:20:03 +0200

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# mena-advisor
+# Project Title
+
+First proof of concept
+
+## Getting Started
+
+1. Install Docker on local machine
+2. Run the docker-compose found at the project's root
+3. 'npm install' to install all the required modules
+4. 'npm migrate' to update ur local db
+5. 'npm start' to start the app
+
+## Running the tests
+
+No test atm
+
+## Deployment
+
+Add additional notes about how to deploy this on a live system
+
+## Versioning
+
+for example, we should take a look:
+
+ [SemVer](http://semver.org/) for versioning. For the versions available.
+
+## Authors
+
+* **THE Marc** - *Initial work* - [PurpleBooth](https://github.com/PurpleBooth)
+
+## License
+
+To be defined
+
+## TEMPLATE
+
+[Template](https://gist.github.com/PurpleBooth/109311bb0361f32d87a2#file-readme-template-md)
---
+++ database.js
@@ -0,0 +1,31 @@
+var util = require('util');
+require('util.promisify').shim();
+var mysql = require('mysql');
+var pool = mysql.createPool({
+    connectionLimit : process.env.DB_CONECTION_POOL,
+    host            : process.env.DB_HOST,
+    user            : process.env.DB_USER,
+    password        : process.env.DB_PASS,
+    database        : process.env.DB_DATABASE,
+  });
+
+  pool.getConnection((err, connection) => {
+    if (err) {
+        if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+            console.error('Database connection was closed.')
+        }
+        if (err.code === 'ER_CON_COUNT_ERROR') {
+            console.error('Database has too many connections.')
+        }
+        if (err.code === 'ECONNREFUSED') {
+            console.error('Database connection was refused.')
+        }
+    }
+    if (connection) connection.release()
+    return
+})
+
+// Promisify for Node.js async/await.
+pool.query = util.promisify(pool.query);
+
+module.exports = pool;
