8572459397b4cc74f0ed263a57164a3127ed770f
imedgar <ed.ga.ru90@gmail.com>
2019-09-30 13:10:04 +0200

    ESLint + code fixes based on ESLint scan

--- app/app.js
+++ app/app.js
@@ -5,7 +5,7 @@ const app = express();
 app.use('/', require('./coordinates/router'));
 
 app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
+  console.log('Example app listening on port 3000!');
 });
 
 module.exports = app;
