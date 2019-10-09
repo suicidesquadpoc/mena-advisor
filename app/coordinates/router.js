337f53b4afacb97d649559fd1f7cf385c399256a
Marc M.B <mmoralesbarreda@gmail.com>
2019-10-09 22:53:40 +0200

    First aproach to express-validator, WIP

--- app/coordinates/router.js
+++ app/coordinates/router.js
@@ -1,11 +1,33 @@
 const coordinateModel = require('./coordinate-model');
 const router = require('express').Router();
+const { param, validationResult } = require('express-validator');
+
+const getCoordinateValidations = [
+  param('id').not().isEmpty().isInt().withMessage('expected an integer value')
+];
+
+const getCoordinate = async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).jsonp(errors.array());
+  } else {
+    const coordinates = await coordinateModel.selectAllCoordinates();
+    res.send(coordinates);
+  }
+};
+
+const postCoordinate = async (req, res) => {
+  const coordinates = await coordinateModel.selectAllCoordinates();
+  res.send(coordinates);
+};
 
 const getAllCoordinates = async (req, res) => {
   const coordinates = await coordinateModel.selectAllCoordinates();
   res.send(coordinates);
 };
 
+router.get('/coordinate/:id', getCoordinateValidations, getCoordinate);
+router.post('/coordinate', postCoordinate);
 router.get('/coordinates', getAllCoordinates);
 
 module.exports = router;
