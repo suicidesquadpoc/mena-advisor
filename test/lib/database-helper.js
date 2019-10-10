b8e2fa2ee4fe8fae7c474311971a97de9b50f981
imedgar <ed.ga.ru90@gmail.com>
2019-10-10 18:43:53 +0200

    github actions testing whole workflow

--- test/lib/database-helper.js
+++ test/lib/database-helper.js
@@ -4,7 +4,7 @@ const db = require('../../app/database');
 const clear = async (table) => {
   let result;
   try {
-    result = await db.query('DELETE FROM `' + table + '`');
+    result = await db.query(`DELETE FROM ${table}`);
   } catch (e) {
     console.error(e);
   }
@@ -15,7 +15,7 @@ const save = async (table, coordinate) => {
   let result;
   try {
     // TODO: customizable columns
-    result = await db.query('INSERT INTO `' + table + '` (latitude, longitude) VALUES (' + coordinate.latitude + ', ' + coordinate.longitude + ')');
+    result = await db.query(`INSERT INTO ${table} (latitude, longitude, time) VALUES (${coordinate.latitude}, ${coordinate.longitude}, NOW())`);
   } catch (e) {
     console.error(e);
   }
